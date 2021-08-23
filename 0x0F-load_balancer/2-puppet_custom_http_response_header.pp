87b9227e19ee6d72d8640f13558f77abbeed945b
OctaveC <octavec@outlook.com>
2021-08-23 21:01:55 +0000

    bonus

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,24 @@
+# Automate the task of creating a custom HTTP header response, but with Puppet
+
+exec { 'update':
+  command => '/usr/bin/apt-get update',
+}
+
+package { 'nginx':
+  ensure  => installed,
+  name    => 'nginx',
+  require => Exec['update'],
+}
+
+file_line { 'add_header':
+  ensure  => present,
+  path    => '/etc/nginx/sites-enabled/default',
+  after   => 'listen 80 default_server;',
+  line    => "add_header X-Served-By ${hostname};",
+  require => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
