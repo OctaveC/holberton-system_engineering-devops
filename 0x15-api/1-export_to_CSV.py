7be76c0dfab48f335e46028d6c1ff64ddd39ea87
OctaveC <octavec@outlook.com>
2021-09-13 20:07:03 +0000

    trying out API exo 2

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+For a given employee ID, returns information about his/her
+TODO list progress.
+Now exporting data in the CSV format!
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    user = requests.get("http://jsonplaceholder.typicode.com/users/" +
+                        argv[1]).json()
+    task_todo = requests.get(
+        "http://jsonplaceholder.typicode.com/todos?userId=" + argv[1]).json()
+    user_id = str(user.get("id"))
+
+    with open(user_id + ".csv", "w") as csv_file:
+        username = user.get("username")
+        user_id = user.get("id")
+
+        for task in task_todo:
+            csv_file.write('"{}","{}","{}","{}"\n'.
+                          format(user_id, username, task.get("completed"),
+                                 task.get("title")))
