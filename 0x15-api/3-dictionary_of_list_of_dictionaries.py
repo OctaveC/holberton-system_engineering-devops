cf2dac6b061719ac37635de2fc87f4691e528209
OctaveC <octavec@outlook.com>
2021-09-13 20:57:11 +0000

    trying out API exo 3 and 4

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -11,14 +11,23 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    user = requests.get("http://jsonplaceholder.typicode.com/users/").json()
-    todo = requests.get('https://jsonplaceholder.typicode.com/todos').json()
-    filename = "todo_all_employees.json"
+    user = requests.get("http://jsonplaceholder.typicode.com/users").json()
 
-    with open(filename, "w") as f:
-        dumpo = {ite2.get("id"): [{'task': i.get('title'),
-                                   'completed': i.get('completed'),
-                                   'username': i.get('username')} for i in todo
-                                  if ite2.get("id") == i.get('userId')]
-                 for ite2 in user}
-        json.dump(dumpo, f)
+    with open("todo_all_employees.json", "w") as jsonfile:
+        user_tasks = {}
+        for user_id in user:
+            username = user_id.get("username")
+            user_id = str(user_id.get("id"))
+            todo = requests.get("http://jsonplaceholder.typicode.com/todos?" +
+                                 "userId=" + user_id).json()
+            task_list = []
+
+            for task in todo:
+                tasks_dict = {"task": task.get("title"),
+                            "completed": task.get("completed"),
+                            "username": username}
+                task_list.append(tasks_dict)
+
+            user_tasks[user_id] = task_list
+
+        json.dump(user_tasks, jsonfile)
