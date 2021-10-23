1d5a96f720bd05b03ba19e12691da3f19282617a
octave collombel <octavec@outlook.com>
2021-10-23 13:52:54 +0200

    puppet lint debugging requests exo 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
-#Fix failling requests
+# Fix failling requests
 
 exec {'using sed to increase limit':
-  path    => ['/usr/local/bin', '/bin'],
   command => "sudo sed -i 's/15/4000/' /etc/default/nginx",
+  path    => '/usr/local/bin/:/bin/',
 }
 
 exec {'nginx restart':
-  path    => ['/bin', '/usr/bin'],
   command => 'sudo service nginx restart',
+  path    => ['/bin', '/usr/bin'],
 }
