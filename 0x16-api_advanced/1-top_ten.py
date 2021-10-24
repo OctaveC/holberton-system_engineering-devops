ea037911d1852e59992843772867db6018c425a5
octave collombel <octavec@outlook.com>
2021-10-24 10:39:14 +0200

    reddit api exo 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Gets the top 10 current hot posts """
+import requests
+import json
+
+def top_ten(subreddit):
+    """ Returns the top 10 hot post a selected subreddit """
+
+    url = 'https://www.reddit.com/'
+
+    try:
+        answer = requests.get(
+            headers={'user-agent': 'custom'},
+            url="{}/r/{}/hot.json?limit=10".format(url, subreddit),
+        )
+
+        data = answer.json()['data']
+
+        for ite in data.get("children"):
+            print(ite.get("data").get("title"))
+
+    except Exception:
+        print(None)
