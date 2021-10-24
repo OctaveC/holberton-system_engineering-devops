add756278112ef7a3ac8b408672c919f372da430
octave collombel <octavec@outlook.com>
2021-10-24 13:10:50 +0200

    reddit api exo bonus test 2

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,40 +1,54 @@
 #!/usr/bin/python3
 """ Prints a count of given keywords from data taken fromm the reddit API """
 import json
+import pprint
+import re
 import requests
 
+def printer(word_list, hot_list):
+    """ Prints our results"""
+    total = {}
+    for word in word_list:
+        total[word] = 0
 
-def count_words(subreddit, word_list, hot_list=[], after=""):
+    for title in hot_list:
+        for word in word_list:
+            total[word] = total[word] +\
+             len(re.findall(r'(?:^| ){}(?:$| )'.format(word), title, re.I))
+
+    total = {k: v for k, v in total.items() if v > 0}
+    words = sorted(list(total.keys()))
+    for word in sorted(words,
+                       reverse=True, key=lambda k: total[k]):
+        print("{}: {}".format(word, total[word]))
+
+
+def count_words(subreddit, word_list, hot_list=[], after=None):
     """ Prints a count of given keywords from reddit API data"""
     url = 'https://www.reddit.com/'
+    params = {'limit': 100}
 
-    try:
-        answer = requests.get(
+    if isinstance(after, str):
+        if after != "STOP":
+            params['after'] = after
+        else:
+            return printer(word_list, hot_list)
+
+    answer = requests.get(
             headers={'User-Agent': 'custom'},
-            url="{}/r/{}/hot.json?after={}".format(url, subreddit, after),
-            allow_redirects=False
-        )
+            url="{}/r/{}/hot.json".format(url, subreddit),
+            allow_redirects=False,
+            params=params
+    )
 
-        if after is None:
-            dict = {}
-            for word in word_list:
-                for hot in hot_list:
-                    if word == hot:
-                        if word not in dict:
-                            dict[word] = 1
-                        else:
-                            dict[word] += 1
-            for word in sorted(dict, key=dict.get, reverse=True):
-                if dict[word]:
-                    print('{}: {}'.format(word, dict[word]))
-            return hot_list
-
-        for ite in answer.json().get('data').get('children'):
-            hot_list += ite.get('data').get('title').lower().split()
-
-        after = answer.json().get('data').get('after')
-        count_words(subreddit, word_list, hot_list, after)
-        return hot_list
-
-    except Exception:
+    if answer.status_code != 200:
         return None
+
+    data = answer.json().get('data', {})
+    after = data.get('after', 'STOP')
+    if not after:
+        after = "STOP"
+    hot_list = hot_list + [post.get('data', {}).get('title')
+                           for post in data.get('children', [])]
+
+    return count_words(subreddit, word_list, hot_list, after)
