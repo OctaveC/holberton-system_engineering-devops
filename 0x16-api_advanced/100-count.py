4819a52738abcbba4ad1db856ae4cf23cdb23292
octave collombel <octavec@outlook.com>
2021-10-24 12:46:58 +0200

    reddit api exo bonus

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" Prints a count of given keywords from data taken fromm the reddit API """
+import json
+import requests
+
+
+def count_words(subreddit, word_list, hot_list=[], after=""):
+    """ Prints a count of given keywords from reddit API data"""
+    url = 'https://www.reddit.com/'
+
+    try:
+        answer = requests.get(
+            headers={'User-Agent': 'custom'},
+            url="{}/r/{}/hot.json?after={}".format(url, subreddit, after),
+            allow_redirects=False
+        )
+
+        if after is None:
+            dict = {}
+            for word in word_list:
+                for hot in hot_list:
+                    if word == hot:
+                        if word not in dict:
+                            dict[word] = 1
+                        else:
+                            dict[word] += 1
+            for word in sorted(dict, key=dict.get, reverse=True):
+                if dict[word]:
+                    print('{}: {}'.format(word, dict[word]))
+            return hot_list
+
+        for ite in answer.json()['data']['children']:
+            hot_list += ite['data']['title'].lower().split()
+
+        after = answer.json()['data']['after']
+        count_words(subreddit, word_list, hot_list, after)
+        return hot_list
+
+    except Exception:
+        return None
